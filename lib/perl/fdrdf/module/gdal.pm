823a3608acc69478dfc629f2bb2d930175d7cc03
Svetlana Zelenina <svetik@theory.asu.ru>
2010-09-07 20:09:03 +0700

    New ‘gdal’ module for ‘fdrdf’.
    lib/perl/fdrdf/module/gdal.pm: New file.
    doc/fdrdf.xml
    (#modules): Added a short description of the ‘gdal’ module.

---
+++ lib/perl/fdrdf/module/gdal.pm
@@ -0,0 +1,74 @@
+### gdal.pm --- FDRDF: GDAL processing module  -*- Perl -*-
+
+### Svetlana Zelenina, 2010
+## This code is in the public domain.
+
+package fdrdf::module::gdal;
+
+use strict;
+use warnings;
+
+use Geo::GDAL;
+use RDF::Redland;
+
+use fdrdf::module;
+use fdrdf::util;
+
+BEGIN {
+    require Exporter;
+    our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+    $VERSION = 0.1;
+    @ISA = qw (Exporter);
+    @EXPORT = qw (&new);
+    @EXPORT_OK = qw (&process_gdal);
+    %EXPORT_TAGS = ();
+}
+
+sub add_rdf {
+    my ($p_ref, $model, $s, $k, $v) = @_;
+    my $cache = $p_ref->{"cache"};
+    unless (exists ($cache->{$k})) {
+        my $uri_s
+            = ($p_ref->{"prefix"} . $k);
+        $cache->{$k} = new RDF::Redland::URINode ($uri_s);
+    }
+    my $p = $cache->{$k};
+    my $o = new RDF::Redland::LiteralNode ($v);
+
+    $model->add_statement ($s, $p, $o);
+}
+
+sub process_gdal {
+    my ($p_ref, $model, $subject, $io) = @_;
+
+    drop_cloexec ($io);
+    my $file = ("/dev/fd/" . $io->fileno ());
+    my $ds = Geo::GDAL::Open ($file, "ReadOnly");
+    my $meta = $ds->GetMetadata ();
+    foreach my $k (keys (%$meta)) {
+        add_rdf ($p_ref, $model, $subject, "$k", "$meta->{$k}");
+    }
+}
+
+sub new {
+    my ($pkg, $e_ref, $config) = @_;
+    my $relation_uri_s
+        = ("uuid:4d0235c4-6c10-4c94-b537-1650395bc50b#"
+           . "gdal.");
+    my %params
+        = ("prefix" => $relation_uri_s,
+           "cache"  => {});
+    my @handle = (\&process_gdal, \%params);
+    module_add_to_tag ($e_ref, "io", \@handle);
+
+    ## .
+    return $e_ref;
+}
+
+1;
+
+## Local variables:
+## indent-tabs-mode: nil
+## fill-column: 72
+## End:
+### gdal.pm ends here
